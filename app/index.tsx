a6aa49fddb603d178de119c82d91505a7b45abe4
3pt14afm <piaflorm5@gmail.com>
2026-08-07 22:51:56 +0800

    Implement code changes to enhance functionality and improve performance

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, Image as RNImage } from "react-native";
 import { router, Redirect } from "expo-router";
 import { useAuthStore } from "@/store/authStore";
+import LibraryLogo from "@/components/logo";
 
 export default function Index() {
   const session = useAuthStore((s) => s.session);
@@ -22,31 +23,50 @@ export default function Index() {
   }
 
   return (
-    <View className="flex-1 justify-center px-6 bg-surface">
-      <Text className="text-3xl font-bold text-shelf-700 text-center mb-1">
-        SHELFTRACK
-      </Text>
-      <Text className="text-muted text-center mb-10">
-        Smart Library Management System
-      </Text>
+    <View className="flex-1 px-6 pb-6 bg-surface">
 
-      <Pressable
-        className="bg-shelf-600 rounded-xl py-4 mb-3"
-        onPress={() => router.push("/(auth)/librarian-login")}
-      >
-        <Text className="text-white text-center font-semibold">
-          Login as Librarian
-        </Text>
-      </Pressable>
-
-      <Pressable
-        className="border border-shelf-600 rounded-xl py-4"
-        onPress={() => router.push("/(auth)/student-login")}
-      >
-        <Text className="text-shelf-600 text-center font-semibold">
-          Login as Student
-        </Text>
-      </Pressable>
+  {/* Centered Middle Section */}
+  <View className="flex-1 justify-center">
+    {/* SVG Logo Container */}
+    <View className="items-center">
+      <LibraryLogo width={200} height={200} />
     </View>
+    <Text className="text-center text-2xl font-extrabold -mt-6 mb-2">SHELFTRACK</Text>
+    <Text className="text-muted text-center mb-10">
+      Smart Library Management System
+    </Text>
+
+    <Pressable
+      className="bg-shelf-600 rounded-xl py-4 mb-3"
+      onPress={() => router.push("/(auth)/librarian-login")}
+    >
+      <Text className="text-white text-center font-semibold">
+        Login as Librarian
+      </Text>
+    </Pressable>
+
+    <Pressable
+      className="border border-shelf-600 rounded-xl py-4"
+      onPress={() => router.push("/(auth)/student-login")}
+    >
+      <Text className="text-shelf-600 text-center font-semibold">
+        Login as Student
+      </Text>
+    </Pressable>
+  </View>
+
+  {/* Pinned Bottom View */}
+<View className="flex-row items-center justify-center gap-2.5 px-6 pt-4">
+  <RNImage 
+    source={require("@/assets/images/steclogo.png")} 
+    className="w-10 h-10"
+    resizeMode="contain"
+  />
+  <Text className="shrink text-xs font-semibold text-center">
+    Ramon M. Durano Sr. Foundation Science and Technology Education Center Library System
+  </Text>
+</View>
+  
+</View>
   );
 }
